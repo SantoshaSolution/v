5a444a9476c07754dd0cb0b0c9e58fdf453ffdeb
unknown <SThapa@cs_v32-300.cs.mum.edu>
2014-12-04 13:57:01 -0600

    Add all file

---
+++ build/generated-sources/ap-source-output/entities/Customer_.java
@@ -0,0 +1,15 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-11-28T14:10:42")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, Long> id;
+    public static volatile SingularAttribute<Customer, String> phone;
+    public static volatile SingularAttribute<Customer, String> name;
+
+}
---
+++ src/java/entities/Customer.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author SThapa
+ */
+@Entity
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
+    @Basic(optional = false)
+    private String name;
+    
+    
+    @Size(max=20, message = "Phone number exceeds max Length")
+    private String phone;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Customer[ id=" + id + " ]";
+    }
+    
+}
